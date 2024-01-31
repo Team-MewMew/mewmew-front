3479ed477f98d01f23967c5ce7e152c483d53584
ubeom kim <kimub1204@gmail.com>
2024-01-31 23:59:26 +0900

    :lipstick: feat: 공통 컴포넌트 버튼 추가

---
+++ src/components/common/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    v-if="char === 'primary'"
+    :type="type"
+    class="bg-primary w-full rounded text-white"
+    @click="handleClick"
+    :class="styles"
+  >
+    <slot />
+  </button>
+  <button
+    v-else
+    :type="type"
+    class="bg-white w-full border border-primary rounded text-primary"
+    @click="handleClick"
+    :class="styles"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  type: 'button' | 'submit';
+  char: 'primary' | 'secondary';
+  styles: string;
+}>();
+
+const emit = defineEmits(['click']);
+
+const handleClick = (event: MouseEvent) => {
+  emit('click', event);
+};
+</script>
+
+<style scoped></style>
